4b47687f0d9face70db7d312aa6cf1c6226d50b8
dennis-huaman <dennis.hyrigoin@gmail.com>
2026-09-16 22:47:54 +0200

    Second Commit

--- code/selector.py
+++ code/selector.py
@@ -1,33 +1,35 @@
+#!/usr/bin/env python3
+
+
 import time
-from traceback import print_tb
 import py_trees
 import random
 
-import os
-
-os.system("clear")
-
 
 class BTAction1(py_trees.behaviour.Behaviour):
+
     def __init__(self, name: str = "BTAction1"):
         super(BTAction1, self).__init__(name)
-        self.logger.debug(f"{self.__class__.__name__}.__init__()")
+        self.logger.debug("%s.__init__()" % (self.__class__.__name__))
 
     def setup(self, **kwargs: int) -> None:
-        self.logger.debug(f"{self.__class__.__name__}.__setup__()")
+        self.logger.debug("%s.setup()" % (self.__class__.__name__))
 
     def initialise(self) -> None:
-        self.logger.debug(f"{self.__class__.__name__}.__initialise__()")
+        self.logger.debug("%s.initialise()" % (self.__class__.__name__))
 
-    def update(self) -> py_trees.commos.Status:
+    def update(self) -> py_trees.common.Status:
+        self.logger.debug("%s.update()" % (self.__class__.__name__))
 
         random_integer = random.randrange(1, 10)
         if random_integer < 3:
-            self.feedback_message = f"Number {random_integer} is less than 3: Success"
+            self.feedback_message = "Number {0} is less than 3: Success ".format(
+                random_integer
+            )
             new_status = py_trees.common.Status.SUCCESS
         else:
-            self.feedback_message = (
-                f"Number {random_integer} is greater than 3: Success"
+            self.feedback_message = "Number {0} is greater than 3: Failure ".format(
+                random_integer
             )
             new_status = py_trees.common.Status.FAILURE
 
@@ -35,25 +37,30 @@ class BTAction1(py_trees.behaviour.Behaviour):
 
 
 class BTAction2(py_trees.behaviour.Behaviour):
+
     def __init__(self, name: str = "BTAction2"):
         super(BTAction2, self).__init__(name)
-        self.logger.debug(f"{self.__class__.__name__}.__init__()")
+        self.logger.debug("%s.__init__()" % (self.__class__.__name__))
 
     def setup(self, **kwargs: int) -> None:
-        self.logger.debug(f"{self.__class__.__name__}.__setup__()")
+        self.logger.debug("%s.setup()" % (self.__class__.__name__))
 
     def initialise(self) -> None:
-        self.logger.debug(f"{self.__class__.__name__}.__initialise__()")
+        self.logger.debug("%s.initialise()" % (self.__class__.__name__))
 
-    def update(self) -> py_trees.commos.Status:
+    def update(self) -> py_trees.common.Status:
+
+        self.logger.debug("%s.update()" % (self.__class__.__name__))
 
         random_integer = random.randrange(1, 10)
         if random_integer < 5:
-            self.feedback_message = f"Number {random_integer} is less than 5: Success"
+            self.feedback_message = "Number {0} is less than 5: Success ".format(
+                random_integer
+            )
             new_status = py_trees.common.Status.SUCCESS
         else:
-            self.feedback_message = (
-                f"Number {random_integer} is greater than 5: Success"
+            self.feedback_message = "Number {0} is greater than 5: Failure ".format(
+                random_integer
             )
             new_status = py_trees.common.Status.FAILURE
 
@@ -61,25 +68,30 @@ class BTAction2(py_trees.behaviour.Behaviour):
 
 
 class BTAction3(py_trees.behaviour.Behaviour):
+
     def __init__(self, name: str = "BTAction3"):
         super(BTAction3, self).__init__(name)
-        self.logger.debug(f"{self.__class__.__name__}.__init__()")
+        self.logger.debug("%s.__init__()" % (self.__class__.__name__))
 
     def setup(self, **kwargs: int) -> None:
-        self.logger.debug(f"{self.__class__.__name__}.__setup__()")
+        self.logger.debug("%s.setup()" % (self.__class__.__name__))
 
     def initialise(self) -> None:
-        self.logger.debug(f"{self.__class__.__name__}.__initialise__()")
+        self.logger.debug("%s.initialise()" % (self.__class__.__name__))
 
-    def update(self) -> py_trees.commos.Status:
+    def update(self) -> py_trees.common.Status:
+
+        self.logger.debug("%s.update()" % (self.__class__.__name__))
 
         random_integer = random.randrange(1, 10)
         if random_integer < 8:
-            self.feedback_message = f"Number {random_integer} is less than 8: Success"
+            self.feedback_message = "Number {0} is less than 8: Success ".format(
+                random_integer
+            )
             new_status = py_trees.common.Status.SUCCESS
         else:
-            self.feedback_message = (
-                f"Number {random_integer} is greater than 8: Success"
+            self.feedback_message = "Number {0} is greater than 8: Failure ".format(
+                random_integer
             )
             new_status = py_trees.common.Status.FAILURE
 
@@ -87,12 +99,56 @@ class BTAction3(py_trees.behaviour.Behaviour):
 
 
 def create_root() -> py_trees.behaviour.Behaviour:
-    root = py_trees.composites.Selector(name="Selector", memory=True)
-    node1 = BTAction1(name="Action1")
+
+    root = py_trees.composites.Selector(name="Selector", memory=False)
+    node1 = BTAction1(name="Action 1")
     root.add_child(node1)
 
-    node2 = BTAction2(name="Action2")
+    node2 = BTAction2(name="Action 2")
     root.add_child(node2)
 
-    node3 = BTAction3(name="Action3")
+    node3 = BTAction3(name="Action 3")
     root.add_child(node3)
+
+    return root
+
+
+def main() -> None:
+
+    py_trees.logging.level = py_trees.logging.Level.DEBUG
+
+    root = create_root()
+
+    tree = py_trees.trees.BehaviourTree(root)
+    tree.setup(timeout=15)
+
+    # El SnapshotVisitor registra que nodos se visitan (se tickean) en cada tick.
+    # Es necesario porque display.unicode_tree(show_status=True) pinta el estado
+    # *almacenado* de TODOS los nodos, incluidos los que no se ejecutaron en el
+    # tick actual (selectores/fallbacks cortan la ejecucion y dejan a los
+    # hermanos de menor prioridad con su status y feedback_message antiguos).
+    snapshot = py_trees.visitors.SnapshotVisitor()
+    tree.visitors.append(snapshot)
+
+    for i in range(1, 6):
+
+        try:
+            print("\n--------- Tick {0} ---------\n".format(i))
+            tree.tick()
+            # Al pasar 'visited' sin 'show_status', solo los nodos realmente
+            # tickeados muestran su status y su mensaje de feedback.
+            print(py_trees.display.unicode_tree(root=root, visited=snapshot.visited))
+            visited_names = [
+                node.name for node in root.iterate() if node.id in snapshot.visited
+            ]
+            print("\nvisited: {0}".format(", ".join(visited_names)))
+            tip = root.tip()
+            print("tip    : {0}".format(tip.name if tip is not None else "None"))
+            time.sleep(1.0)
+        except KeyboardInterrupt:
+            break
+    print("\n")
+
+
+if __name__ == "__main__":
+    main()
